3904c519021d098d3f2a6aa1c14e0748f8b6b645
Jasonmmill <jasonmatthewmill@gmail.com>
2025-03-02 21:41:00 -0500

    Task 3 - Implemented Dynamic Inventory List

--- cc_12.js
+++ cc_12.js
@@ -1,25 +1,50 @@
 //Task 1: Business Dashboard - DOM Element Selection and Creation
 const dashboardById = document.getElementById("dashboard")
 const dashboardByQuery = document.querySelector("#dashboard")
-
 const revenueCard = document.createElement("div")
-
 revenueCard.setAttribute("class", "metric-card")
 revenueCard.setAttribute("id", "revenueCard")
-
 revenueCard.innerHTML = `
     <h3>Revenue</h3>
     <p>$0</p>
 `
-
+const profitCard = document.createElement("div")
+profitCard.setAttribute("class", "metric-card")
+profitCard.setAttribute("id", "profitCard")
+profitCard.innerHTML = `
+    <h3>Profit</h3>
+    <p>$0</p>
+`
+const expensesCard = document.createElement("div")
+expensesCard.setAttribute("class", "metric-card")
+expensesCard.setAttribute("id", "expensesCard")
+expensesCard.innerHTML = `
+    <h3>Expenses</h3>
+    <p>$0</p>
+`
 dashboardById.appendChild(revenueCard)
+dashboardById.appendChild(profitCard)
+dashboardById.appendChild(expensesCard)
 
 // Task 2: Updating Dashbaord Metrics - Working with NodeLists and Arrays
-const allCards = document.querySelectorAll("metric-card")
+const allCards = document.querySelectorAll(".metric-card")
 const allCardsArray = [...allCards]
 
 allCardsArray.forEach(card => {
-    const revenue = card.querySelector("h3")
-    revenue.innerText += " - Refreshed"
-    card.style.backgroundColor = "lime"
+    card.innerHTML += " - Refreshed"
+    card.style.backgroundColor = "pink"
 })
+
+// Task 3: Dynamic Inventory Management - Adding & Removing Items
+function addListItem(productName) {
+    const inventoryList = document.getElementById("inventoryList")
+    const listItem = document.createElement("li")
+    listItem.setAttribute("class", "product-item")
+    listItem.setAttribute("data-attribute", productName)
+    listItem.textContent = productName
+
+    listItem.addEventListener("click", () => {
+        inventoryList.removeChild(listItem)
+    })
+    inventoryList.appendChild(listItem)
+}
